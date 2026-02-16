e0bd5f098ad9fa6d8501804b43a8e9607f7cd47c
sigurdovereng <sigurdove@gmail.com>
2026-02-16 14:05:03 +0100

    Push

---
+++ src/main/java/com/stats/verketbooking/VerketbookingApplication.java
@@ -0,0 +1,13 @@
+package com.stats.verketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerketbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerketbookingApplication.class, args);
+	}
+
+}
