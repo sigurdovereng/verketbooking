107648e4d6f4922ad865110f77d605c9a4d1cd25
sharujanlingam <sharujans2@gmail.com>
2026-03-02 12:01:31 +0100

    Lager service, repo og controller for reservation

--- src/main/java/com/stats/verketbooking/service/ReservationService.java
+++ src/main/java/com/stats/verketbooking/service/ReservationService.java
@@ -1,4 +1,29 @@
 package com.stats.verketbooking.service;
 
+import com.stats.verketbooking.model.Reservation;
+import com.stats.verketbooking.repository.ReservationRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReservationService {
+
+    private final ReservationRepo reservationRepo;
+
+    public ReservationService(ReservationRepo reservationRepo) {
+        this.reservationRepo = reservationRepo;
+    }
+
+    public List<Reservation> getAll() {
+        return reservationRepo.findAll();
+    }
+
+    public Reservation save(Reservation reservation) {
+        return reservationRepo.save(reservation);
+    }
+
+    public void delete(Long id) {
+        reservationRepo.deleteById(id);
+    }
 }
