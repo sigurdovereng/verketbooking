1991ff1b6280c5904c285f22620a09f34fab7d00
AdrianSF <adrians-f@hotmail.com>
2026-04-14 16:23:46 +0200

    Fikser sms

--- src/main/java/com/stats/verketbooking/service/SmsService.java
+++ src/main/java/com/stats/verketbooking/service/SmsService.java
@@ -1,7 +1,9 @@
 package com.stats.verketbooking.service;
 
+import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,17 @@ public class SmsService {
 
     @Value("${twilio.from-number}")
     private String fromNumber;
+
+    @Value("${twilio.account-sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth-token}")
+    private String authToken;
+
+    @PostConstruct
+    public void initTwilio() {
+        Twilio.init(accountSid, authToken);
+    }
 
     public String sendSms(String toNumber, String body) {
         Message message = Message.creator(
@@ -21,13 +34,4 @@ public class SmsService {
         return message.getSid();
     }
 
-    public String sendNextUpSms(String toNumber, String guestName, String gameName) {
-        String body = "Hi " + guestName + "! Your turn for " + gameName + " starts in about 5 minutes.";
-        return sendSms(toNumber, body);
-    }
-
-    public String sendEndingSoonSms(String toNumber, String guestName, String gameName) {
-        String body = "Hi " + guestName + "! Your turn for " + gameName + " ends in about 5 minutes.";
-        return sendSms(toNumber, body);
-    }
 }
