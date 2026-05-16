dbdf33544d4140a60d7b1247781ac4ad0ee07d63
AdrianSF <adrians-f@hotmail.com>
2026-05-16 11:30:14 +0200

    Endret litt på URL tillatelse i CORS for ny Render deploy

--- src/main/java/com/stats/verketbooking/config/SecurityConfig.java
+++ src/main/java/com/stats/verketbooking/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:3001",
-                "https://verketbooking.onrender.com"
+                "https://vaerketbooking.onrender.com"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT"));
         config.setAllowedHeaders(List.of("*"));
